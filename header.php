273603c30b885c890cadcc83f92f123195a5c52e
mkrms <new.fountain@docomo.ne.jp>
2024-05-13 00:55:36 +0900

    add ogp setting

--- header.php
+++ header.php
@@ -6,6 +6,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>UNOTAME</title>
 
+  <meta property="og:url" content="https://unotame.com/">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="UNOTAME">
+  <meta property="og:description" content="「あなた」のために。">
+  <meta property="og:image" content="http://unotame.com/wp-content/themes/unotame/assets/img/unotame-logo-square.png">
+
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/destyle.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
 
